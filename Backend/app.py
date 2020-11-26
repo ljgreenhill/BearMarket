9efefd85bfef8892f0166dacea94ef98549d01d8
lgreenhill <Rabbit@456>
2020-11-25 22:36:41 -0500

    sell item- need to fix active default

--- Backend/app.py
+++ Backend/app.py
@@ -40,6 +40,50 @@ def get_posts():
 def get_active_posts():
     return success_response([p.serialize() for p in Post.query.filter_by(active=True)])
 
+@app.route("/api/users/", methods=["POST"])
+def create_user():
+    body = json.loads(request.data)
+    if(body.get('email') is None):
+        return failure_response('No email provided')
+    new_user = User(email=body.get('email'), bio=body.get('bio'))
+    db.session.add(new_user)
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
+
+@app.route("/api/<int:seller_id>/post/", methods=["POST"])
+def create_post(seller_id):
+    body = json.loads(request.data)
+    seller = User.query.filter_by(id=seller_id).first()
+    if seller is None:
+        return failure_response('User not found')
+    if(body.get('title') is None):
+        return failure_response('No title provided')
+    new_post = Post(title=body.get('title'), description=body.get('description'), seller=seller_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return success_response(new_post.serialize(), 201)
+
+@app.route("/api/posts/<int:post_id>/sell/<int:buyer_id>/", methods=["POST"])
+def sell_item(post_id, buyer_id):
+    post = Post.query.filter_by(id=post_id).first()
+    if post is None:
+        return failure_response('Item not found')
+    if not post.active:
+        return failure_response('Item inactive')
+    buyer = User.query.filter_by(id=buyer_id).first()
+    if buyer is None:
+        return failure_response('User not found')
+    if buyer.id == post.seller:
+        return failure_response('Buyer and seller are the same')
+    post.active = False
+    return success_response(post.serialize())
+
+@app.route("/api/users/<int:user_id>/")
+def get_user(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response('User not found')
+    return success_response(user.serialize())
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
--- Backend/db.py
+++ Backend/db.py
@@ -2,13 +2,6 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-association_post_seller = db.Table(
-    'association_post_seller', 
-    db.Model.metadata,
-    db.Column('post_id', db.Integer, db.ForeignKey('post.id')),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'))
-)
-
 association_post_buyer = db.Table(
     'association_post_buyer', 
     db.Model.metadata,
@@ -26,29 +19,35 @@ association_post_interested = db.Table(
 class Post(db.Model):
     __tablename__ = 'post'
     id = db.Column(db.Integer, primary_key = True)
-    active = (db.Boolean, nullable=True)
+    active = db.Column(db.Boolean, default=True)
     #TODO item images
-    title = (db.String, nullable=False)
-    description = (db.String, nullable=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=True)
+    buyers = db.relationship('User', secondary=association_post_buyer, back_populates='buyer_posts')
+    seller = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)    
+    interested = db.relationship('User', secondary=association_post_interested, back_populates='interested_posts')
 
     def __innit__(self, **kwargs):
-        self.active = False
+        self.active = True
         self.title = kwargs.get('title', '')
         self.description = kwargs.get('description', '')
+        self.seller = kwargs.get('seller')
     
     def serialize(self):
         return {
+            'id': self.id,
             'title': self.title,
-            'description': self.description
+            'description': self.description,
+            'active': self.active
         }
 
 class User(db.Model):
     __tablename__='user'
     id = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String, nullable=False)
-    bio = (db.String, nullable=True)
-    #TODO: store image as profile_pic
-    seller_posts = db.relationship('Post', secondary=association_post_seller, back_populates='sellers')
+    bio = db.Column(db.String, nullable=True)
+    #TODO: store image as profile_pic    
+    seller_posts = db.relationship('Post', cascade='delete')
     buyer_posts = db.relationship('Post', secondary=association_post_buyer, back_populates='buyers')
     interested_posts = db.relationship('Post', secondary=association_post_interested, back_populates='interested')
 
@@ -61,19 +60,22 @@ class User(db.Model):
         bought = []
         sold = []
         selling = []
-
-        for post in self.interested_posts:
+        for post in self.interested_posts and post.serialize().get('active'):
             interested.append(post.serialize())
-        for post in self.seller_posts:
+        for post in self.seller_posts:            
             if(post.serialize().get('active')):
-                selling.append(post)
+                selling.append(post.serialize())
             else:
-                sold.append(post)
+                sold.append(post.serialize())
         for post in self.buyer_posts:
             if not post.serialize().get('active'):
-                bought.append(post)
-
+                bought.append(post.serialize())
         return {
+            'id': self.id,
             'email': self.email,
-            'bio': self.bio
+            'bio': self.bio,
+            'interested': interested,
+            'selling': selling,
+            'sold': sold,
+            'bought': bought
         }
