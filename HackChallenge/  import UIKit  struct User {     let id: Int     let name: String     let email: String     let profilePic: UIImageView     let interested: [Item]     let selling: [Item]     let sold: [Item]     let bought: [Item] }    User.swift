9a9072b6a15bb4735e04e506340e5b01e4f7338b
Edward <yuere-mac@RachYues-MacBook-Pro.local>
2020-12-12 14:11:05 -0600

    Rachel's

---
+++ HackChallenge/  import UIKit  struct User {     let id: Int     let name: String     let email: String     let profilePic: UIImageView     let interested: [Item]     let selling: [Item]     let sold: [Item]     let bought: [Item] }    User.swift
@@ -0,0 +1,12 @@
+//
+//    import UIKit  struct User {     let id: Int     let name: String     let email: String     let profilePic: UIImageView     let interested: [Item]     let selling: [Item]     let sold: [Item]     let bought: [Item] }    User.swift
+//  HackChallenge
+//
+//  Created by Edward on 12/12/20.
+//
+
+import UIKit
+
+class __import_UIKit__struct_User_______let_id__Int_____let_name__String_____let_email__String_____let_profilePic__UIImageView_____let_interested___Item______let_selling___Item______let_sold___Item______let_bought___Item_______User: NSObject {
+
+}
