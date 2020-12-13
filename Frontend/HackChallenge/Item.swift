747923ecdabbd222cdcb3c2933d3e2ba97214fa6
lgreenhill <Rabbit@456>
2020-12-13 13:55:36 -0500

    restoring files

---
+++ Frontend/HackChallenge/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  HackChallenge
+//
+//  Created by Peter Huo on 11/30/20.
+//
+import Foundation
+
+struct Item {
+    var itemImage: String
+    var itemName: String
+    var userImage: String
+    var userName: String
+    var price: String
+}
